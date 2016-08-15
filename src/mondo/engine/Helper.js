c76eeab21f12b2810acc45483fcc888c8abb2d85
oprogramador <uomodislesia@gmail.com>
2016-08-15 21:16:39 +0200

    Helper other methods

--- src/mondo/engine/Helper.js
+++ src/mondo/engine/Helper.js
@@ -1,4 +1,6 @@
+import NumberToken from 'plezuro-js-es6/src/mondo/token/NumberToken.js';
 import SymbolToken from 'plezuro-js-es6/src/mondo/token/SymbolToken.js';
+import WhiteSpaceToken from 'plezuro-js-es6/src/mondo/token/WhiteSpaceToken.js';
 
 const addNullBetweenSemicolonAndBracketClose = Symbol();
 const removeComaBeforeBracketClose = Symbol();
@@ -26,11 +28,40 @@ export default class Helper {
   }
 
   [removeComaBeforeBracketClose]() {
-
+    for (
+      let token = this.tokenizer.hardReset();
+      token !== null;
+      token = this.tokenizer.hardNext()
+    ) {
+      if (token.getOriginalText() === ',') {
+        const next = this.tokenizer.getNextNotBlank();
+        if (next === null || next.isClosingToken()) {
+          this.tokenizer.resetToThis();
+          this.tokenizer.replaceToken(
+            new WhiteSpaceToken().setOriginalText('')
+          );
+        }
+      }
+    }
   }
 
   [addZeroBeforeMinus]() {
-
+    for (
+      let token = this.tokenizer.hardReset();
+      token !== null;
+      token = this.tokenizer.hardNext()
+    ) {
+      if (token.getOriginalText() === '-') {
+        const previous = this.tokenizer.getPreviousNotBlank();
+        if (
+          previous === null ||
+          previous.isOpeningToken() ||
+          previous.isDelimiter()
+        ) {
+          this.tokenizer.insertAfter(new NumberToken().setText('0'));
+        }
+      }
+    }
   }
 
   process() {
