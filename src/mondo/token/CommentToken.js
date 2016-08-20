c853309809489ed1bd3c8a897e426624137fe9a8
oprogramador <uomodislesia@gmail.com>
2016-08-20 16:13:32 +0200

    CommentToken methods

--- src/mondo/token/CommentToken.js
+++ src/mondo/token/CommentToken.js
@@ -1,5 +1,17 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
-export default class CommentToken extends Token {
+const COMMENT_SYMBOL_LENGTH = 2;
 
+export default class CommentToken extends Token {
+  isBlank() {
+    return true;
+  }
+
+  getRegex() {
+    return '\\/\\/.*';
+  }
+
+  doConvert() {
+    this.setText(`/*${this.originalText.substring(COMMENT_SYMBOL_LENGTH)}*/`);
+  }
 }
