e292ec60a1f79502b94782e6d8f18fa62cb73c01
oprogramador <uomodislesia@gmail.com>
2016-08-20 16:22:01 +0200

    DeclarationToken methods

--- src/mondo/token/DeclarationToken.js
+++ src/mondo/token/DeclarationToken.js
@@ -1,5 +1,27 @@
+import OperatorToken from 'plezuro-js-es6/src/mondo/token/OperatorToken.js';
+import SymbolToken from 'plezuro-js-es6/src/mondo/token/SymbolToken.js';
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
-export default class DeclarationToken extends Token {
+const DECLARATION_SYMBOL_LENGTH = 1;
 
+export default class DeclarationToken extends Token {
+  isBlank() {
+    return false;
+  }
+
+  isEntity() {
+    return true;
+  }
+
+  getRegex() {
+    return '\\$[A-Za-z_]+[A-Za-z_0-9]*';
+  }
+
+  doConvert(tokenizer) {
+    tokenizer.insertBefore(new SymbolToken().setText(
+      `var ${this.originalText.substring(DECLARATION_SYMBOL_LENGTH)}`
+    ));
+    tokenizer.insertAfter(new OperatorToken().setText(';'));
+    this.setText(this.originalText.substring(DECLARATION_SYMBOL_LENGTH));
+  }
 }
