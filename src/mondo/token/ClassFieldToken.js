b4e2cf65022afa13a06686e5a39c8f094537d753
oprogramador <uomodislesia@gmail.com>
2016-08-20 16:09:39 +0200

    ClassFieldToken methods

--- src/mondo/token/ClassFieldToken.js
+++ src/mondo/token/ClassFieldToken.js
@@ -1,5 +1,20 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
 export default class ClassFieldToken extends Token {
+  isBlank() {
+    return false;
+  }
 
+  isEntity() {
+    return true;
+  }
+
+  getRegex() {
+    return '[A-Za-z_]+[A-Za-z_0-9]*::[A-Za-z_]+[A-Za-z_0-9]*';
+  }
+
+  doConvert() {
+    const split = this.text.split('::');
+    this.setText(`${split[0]}['fields']['split[1]']`);
+  }
 }
