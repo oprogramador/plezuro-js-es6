79d92b9d8e1f84d796cc631027ded4e04e048696
oprogramador <uomodislesia@gmail.com>
2016-08-21 01:06:38 +0200

    NewLineToken methods

--- src/mondo/token/NewLineToken.js
+++ src/mondo/token/NewLineToken.js
@@ -1,5 +1,16 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
 export default class NewLineToken extends Token {
+  isBlank() {
+    return true;
+  }
 
+  constructor(lineNr, x) {
+    super();
+    this.lineNr = lineNr;
+    this.begX = x;
+    this.endX = x;
+    this.originalText = '\n';
+    this.text = '\n';
+  }
 }
