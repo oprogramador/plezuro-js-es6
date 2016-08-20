93a0f15a6fa1282ff646fef318e0c65e61754c1f
oprogramador <uomodislesia@gmail.com>
2016-08-20 16:27:10 +0200

    FunctionToken and FunctionEndToken methods

--- src/mondo/token/FunctionEndToken.js
+++ src/mondo/token/FunctionEndToken.js
@@ -1,5 +1,12 @@
+import FunctionToken from 'plezuro-js-es6/src/mondo/token/FunctionToken.js';
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
 export default class FunctionEndToken extends Token {
+  isClosingToken() {
+    return true;
+  }
 
+  getOpenClass() {
+    return FunctionToken;
+  }
 }
--- src/mondo/token/FunctionToken.js
+++ src/mondo/token/FunctionToken.js
@@ -1,5 +1,7 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
 export default class FunctionToken extends Token {
-
+  isOpeningToken() {
+    return true;
+  }
 }
