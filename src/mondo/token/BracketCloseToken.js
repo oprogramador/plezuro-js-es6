c8135ca52f8b71cab7634dda327dd594f75128c8
oprogramador <uomodislesia@gmail.com>
2016-08-20 15:44:49 +0200

    BracketOpenToken and BracketCloseToken methods

--- src/mondo/token/BracketCloseToken.js
+++ src/mondo/token/BracketCloseToken.js
@@ -1,7 +1,12 @@
+import BracketOpenToken from 'plezuro-js-es6/src/mondo/token/BracketOpenToken.js';
 import BracketToken from 'plezuro-js-es6/src/mondo/token/BracketToken.js';
 
 export default class BracketCloseToken extends BracketToken {
   isClosingToken() {
     return true;
+  }
+
+  getOpenClass() {
+    return BracketOpenToken;
   }
 }
--- src/mondo/token/BracketOpenToken.js
+++ src/mondo/token/BracketOpenToken.js
@@ -1,7 +1,41 @@
 import BracketToken from 'plezuro-js-es6/src/mondo/token/BracketToken.js';
+import OperatorToken from 'plezuro-js-es6/src/mondo/token/OperatorToken.js';
+import SymbolToken from 'plezuro-js-es6/src/mondo/token/SymbolToken.js';
+
+const insertFunctionCallEventually = Symbol();
 
 export default class BracketOpenToken extends BracketToken {
   isOpeningToken() {
     return true;
+  }
+
+  [insertFunctionCallEventually](tokenizer) {
+    tokenizer.resetToThis();
+    const previous = tokenizer.getPreviousNotBlank();
+    if (
+      previous === null ||
+      previous.isOpeningToken() ||
+      previous instanceof OperatorToken
+    ) {
+      return false;
+    }
+    const preprevious = tokenizer.getPreviousNotBlank();
+    if (
+      preprevious !== null &&
+      preprevious.getText() === '.'
+    ) {
+      return false;
+    }
+
+    tokenizer.resetToThis();
+    tokenizer.insertBefore(new SymbolToken().setText('.call'));
+
+    return true;
+  }
+
+  doConvert(tokenizer) {
+    super.doConvert(tokenizer);
+    tokenizer.resetToThis();
+    insertFunctionCallEventually(tokenizer);
   }
 }
