e0aa6f881f7227a7ee49b6c9a7ea2e4bd7afd3cc
oprogramador <uomodislesia@gmail.com>
2016-08-20 16:43:48 +0200

    MultiLineCommentToken methods

--- src/mondo/token/MultiLineCommentToken.js
+++ src/mondo/token/MultiLineCommentToken.js
@@ -1,5 +1,15 @@
 import MultiLineToken from 'plezuro-js-es6/src/mondo/token/MultiLineToken.js';
 
 export default class MultiLineCommentToken extends MultiLineToken {
+  isBlank() {
+    return true;
+  }
 
+  getRegex() {
+    return '\\/\\*.*';
+  }
+
+  getEndRegex() {
+    return '.*\\*\\/';
+  }
 }
