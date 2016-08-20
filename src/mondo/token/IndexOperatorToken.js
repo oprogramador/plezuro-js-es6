f691de3c67e7fa7401b92441d337d6f2b7876796
oprogramador <uomodislesia@gmail.com>
2016-08-20 16:40:38 +0200

    IndexOperatorToken methods

--- src/mondo/token/IndexOperatorToken.js
+++ src/mondo/token/IndexOperatorToken.js
@@ -1,5 +1,30 @@
 import SquareBracketOpenToken from 'plezuro-js-es6/src/mondo/token/SquareBracketOpenToken.js';
 
-export default class IndexOperatorToken extends SquareBracketOpenToken {
+const NULL_INDEX = 0;
+const INITIAL_INDEX = 1;
 
+export default class IndexOperatorToken extends SquareBracketOpenToken {
+  getMatchingCloseBracket(tokenizer) {
+    let counter = INITIAL_INDEX;
+    for (
+      let token = tokenizer.getNext();
+      token !== null;
+      token = tokenizer.getNext()
+    ) {
+      if (token.getOriginalText() === '[') {
+        counter++;
+      }
+      if (token.getOriginalText() === ']') {
+        counter--;
+      }
+      if (counter === NULL_INDEX) {
+        return token;
+      }
+    }
+
+    return null;
+  }
+
+  matchOperatorMethod() {
+  }
 }
