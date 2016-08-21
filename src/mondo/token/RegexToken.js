3a0bad128f1da47f4c17af4baa53a918607a5088
oprogramador <uomodislesia@gmail.com>
2016-08-21 23:26:49 +0200

    RegexToken methods

--- src/mondo/token/RegexToken.js
+++ src/mondo/token/RegexToken.js
@@ -1,5 +1,30 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
-export default class RegexToken extends Token {
+const REGEX_SYMBOL_BEGIN_LENGTH = 2;
+const REGEX_SYMBOL_END_LENGTH = 1;
 
+export default class RegexToken extends Token {
+  isBlank() {
+    return false;
+  }
+
+  isEntity() {
+    return true;
+  }
+
+  getRegex() {
+    return "r(('([^']|(''))*')|(\"([^\"]|(\"\"))*\"))";
+  }
+
+  doConvert() {
+    const result = this.getOriginalText().substring(
+      REGEX_SYMBOL_BEGIN_LENGTH,
+      this.getOriginalText().length - REGEX_SYMBOL_END_LENGTH
+    )
+    .replace(/\\\\/g, '\\\\\\\\')
+    .replace(/('')|'/g, '\\\\\'')
+    .replace(/("")|"/g, '\\\\"')
+    ;
+    this.setText(`new RegExp("${result}")`);
+  }
 }
