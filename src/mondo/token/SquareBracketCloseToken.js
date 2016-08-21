3ebf691f2b6484c187e079c5353de2c5f9eb5e5b
oprogramador <uomodislesia@gmail.com>
2016-08-21 23:29:28 +0200

    SquareBracketCloseToken method

--- src/mondo/token/SquareBracketCloseToken.js
+++ src/mondo/token/SquareBracketCloseToken.js
@@ -1,7 +1,12 @@
 import BracketToken from 'plezuro-js-es6/src/mondo/token/BracketToken.js';
+import SquareBracketOpenToken from 'plezuro-js-es6/src/mondo/token/SquareBracketOpenToken.js';
 
 export default class SquareBracketCloseToken extends BracketToken {
   isClosingToken() {
     return true;
+  }
+
+  getOpenClass() {
+    return SquareBracketOpenToken;
   }
 }
