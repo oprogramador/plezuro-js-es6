1d4c0dd4d707dab80f7ebca036ff76fd432a814c
oprogramador <uomodislesia@gmail.com>
2016-08-22 01:31:40 +0200

    BiOperatorToken methods

--- src/mondo/token/BiOperatorToken.js
+++ src/mondo/token/BiOperatorToken.js
@@ -1,5 +1,202 @@
+import BracketToken from 'plezuro-js-es6/src/mondo/token/BracketToken.js';
 import OperatorToken from 'plezuro-js-es6/src/mondo/token/OperatorToken.js';
 
-export default class BiOperatorToken extends OperatorToken {
+const INITIAL_ORDER = -1;
+const MAX_ORDER_FACTOR = 2;
+const STRING_BEGIN_INDEX = 0;
+const COMPOSITE_OPERATOR_EQUALS_SIGN_LENGTH = 1;
 
+const delimiterTokens = new Set([
+  ';',
+  ',',
+]);
+
+const tokensAllowedAtBegin = new Set([
+  '+',
+  '-',
+]);
+
+const compositeOperators = new Set([
+  '+=',
+  '-=',
+  '*=',
+  '/=',
+  '^=',
+  '&=',
+  '|=',
+  '%=',
+  '.=',
+]);
+
+const arithmeticTokens = new Set([
+  '+',
+  '-',
+  '%',
+  '*',
+  '/',
+  '^',
+  '.',
+]);
+
+const possibleTokens = new Set([
+  ';',
+  ',',
+  ':=',
+  '=',
+  '+=',
+  '-=',
+  '*=',
+  '/=',
+  '^=',
+  '&=',
+  '|=',
+  '%=',
+  '.=',
+  '~~',
+  '<->',
+  '<<',
+  '>>',
+  '?',
+  '|',
+  '&',
+  '<=>',
+  '>=',
+  '>',
+  '<=',
+  '<',
+  '!=',
+  '==',
+  '!==',
+  '===',
+  '=~',
+  '!~',
+  '+',
+  '-',
+  '%',
+  '*',
+  '/',
+  '^',
+  '^^',
+  '.',
+  '..',
+  ':',
+]);
+
+const operatorsToOverload = new Set([
+  '<<',
+  '>>',
+  '|',
+  '&',
+  '=~',
+  '!~',
+  '+',
+  '-',
+  '%',
+  '*',
+  '/',
+  '^',
+  '..',
+  ':',
+  '?',
+]);
+
+let operatorOrder = null;
+
+const init = () => {
+  operatorOrder = new Map();
+  possibleTokens.forEach((token, i) => operatorOrder.set(i, token));
+};
+init();
+
+export default class BiOperatorToken extends OperatorToken {
+  constructor() {
+    super();
+    this.order = INITIAL_ORDER;
+  }
+
+  setOrder(value) {
+    this.order = value;
+
+    return this;
+  }
+
+  getOrder() {
+    if (this.order !== INITIAL_ORDER) {
+      return this.order;
+    }
+
+    return this.operatorOrder.get(this.originalText);
+  }
+
+  getOnlyPossibleTokens() {
+    return this.possibleTokens;
+  }
+
+  isArithmetic() {
+    return arithmeticTokens.has(this.originalText);
+  }
+
+  isAllowedAtBegin() {
+    return tokensAllowedAtBegin.has(this.originalText);
+  }
+
+  isDelimiter() {
+    return delimiterTokens.has(this.originalText);
+  }
+
+  getOrderMaxNumber() {
+    return possibleTokens.size * MAX_ORDER_FACTOR;
+  }
+
+  getFunctionMap() {
+    return {};
+  }
+
+  replaceComposeOperator(tokenizer) {
+    let operator = tokenizer.getCurrent().getOriginalText();
+    operator = operator.substring(
+      STRING_BEGIN_INDEX,
+      operator.length - COMPOSITE_OPERATOR_EQUALS_SIGN_LENGTH
+    );
+    tokenizer.getCurrent().setText('=');
+    tokenizer.getPreviousNotBlank();
+    const group = tokenizer.getLastGroup();
+    tokenizer.resetToThis();
+    group.forEach((token) => tokenizer.insertAfter(token));
+    tokenizer.insertAfter(
+      new BiOperatorToken().setOriginalText(operator).setOrder(this.getOrder())
+    );
+  }
+
+  preConvert(tokenizer) {
+    if (compositeOperators.has(tokenizer.getCurrent().getOriginalText())) {
+      this.replaceComposeOperator(tokenizer);
+    }
+  }
+
+  matchOperatorMethod(tokenizer) {
+    if (!operatorsToOverload.has(tokenizer.getCurrent().getOriginalText())) {
+      return;
+    }
+    const myOrder = tokenizer.getCurrent().getOrder();
+    tokenizer.getCurrent().setText(`['${tokenizer.getCurrent().getOriginalText()}'](`);
+    for (
+      let token = tokenizer.getNextAtSameBracketLevel();
+      token !== null;
+      token = tokenizer.getNextAtSameBracketLevel()
+    ) {
+      if (
+        token.isClosingToken ||
+        (
+          token instanceof BiOperatorToken &&
+          token.getOrder() <= myOrder
+        )
+      ) {
+        tokenizer.insertBefore(BracketToken.getOperatorBracketClose());
+
+        return;
+      }
+    }
+    tokenizer.insertBefore(BracketToken.getOperatorBracketClose());
+  }
 }
