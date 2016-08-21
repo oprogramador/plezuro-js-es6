c912ab867d01239a60c6f256d4751c19ab73fe8e
oprogramador <uomodislesia@gmail.com>
2016-08-22 00:43:14 +0200

    WhiteSpaceToken methods

--- src/mondo/token/WhiteSpaceToken.js
+++ src/mondo/token/WhiteSpaceToken.js
@@ -1,5 +1,11 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
 export default class WhiteSpaceToken extends Token {
+  isBlank() {
+    return true;
+  }
 
+  getRegex() {
+    return '[ \\\t]+';
+  }
 }
