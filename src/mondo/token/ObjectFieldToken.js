e40f3d172ecd01d58c37266b5b7ff3e9ad2f84a5
oprogramador <uomodislesia@gmail.com>
2016-08-21 21:53:20 +0200

    ObjectFieldToken methods

--- src/mondo/token/ObjectFieldToken.js
+++ src/mondo/token/ObjectFieldToken.js
@@ -1,5 +1,21 @@
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
-export default class ObjectFieldToken extends Token {
+const OBJECT_FIELD_SYMBOL_LENGTH = 1;
 
+export default class ObjectFieldToken extends Token {
+  isBlank() {
+    return false;
+  }
+
+  isEntity() {
+    return true;
+  }
+
+  getRegex() {
+    return '@[A-Za-z_]+[A-Za-z_0-9]*';
+  }
+
+  doConvert() {
+    this.setText(`this['fields']['${this.text.substring(OBJECT_FIELD_SYMBOL_LENGTH)}']`);
+  }
 }
