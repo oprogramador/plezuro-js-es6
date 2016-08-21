d2361a342fa2d44e802ce03134802d61354aa6b2
oprogramador <uomodislesia@gmail.com>
2016-08-21 23:33:10 +0200

    StringToken methods

--- src/mondo/token/StringToken.js
+++ src/mondo/token/StringToken.js
@@ -1,5 +1,22 @@
+import BracketToken from 'plezuro-js-es6/src/mondo/token/BracketToken.js';
 import Token from 'plezuro-js-es6/src/mondo/token/Token.js';
 
 export default class StringToken extends Token {
+  isBlank() {
+    return false;
+  }
 
+  isEntity() {
+    return true;
+  }
+
+  getRegex() {
+    return "('')|('.*?([^\\\\]|(\\\\\\\\))')|(\"\")|(\".*?([^\\\\]|(\\\\\\\\))\")";
+  }
+
+  doConvert(tokenizer) {
+    tokenizer.insertBefore(BracketToken.getOperatorBracketOpen().setText('(new String('));
+    tokenizer.resetToThis();
+    tokenizer.insertAfter(BracketToken.getOperatorBracketClose().setText('))'));
+  }
 }
